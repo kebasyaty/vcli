4202ecfffb10a77429e6553101033386691181a4
kebasyaty <kebasyaty@gmail.com>
2024-05-14 15:58:04 +0300

    update method - run

--- src/vizbor-cli/cmd.cr
+++ src/vizbor-cli/cmd.cr
@@ -11,6 +11,18 @@ module VizborCLI
     OptionParser.parse(args) do |parser|
       parser.on("-v", "--version", "Print version") { print_version }
       parser.on("-h", "--help", "Show this help") { print_help(parser) }
+      #
+      parser.missing_option do |option_flag|
+        STDERR.puts "ERROR: #{option_flag} is missing something."
+        STDERR.puts ""
+        STDERR.puts parser
+        exit(1)
+      end
+      parser.invalid_option do |option_flag|
+        STDERR.puts "ERROR: #{option_flag} is not a valid option."
+        STDERR.puts parser
+        exit(1)
+      end
     end
   end
 
