3dd43a3f77317376fe8b9bdfb92b7026e3aadd89
kebasyaty <kebasyaty@gmail.com>
2024-05-14 15:39:38 +0300

    update

--- src/vizbor-cli/cmd.cr
+++ src/vizbor-cli/cmd.cr
@@ -1,7 +1,26 @@
 require "option_parser"
 
 module VizborCLI
-  def self.run(args = ARGV) : Nil
-    # ...
+  extend self
+
+  def run(args = ARGV) : Nil
+    parse_args args
+  end
+
+  def parse_args(args)
+    OptionParser.parse(args) do |parser|
+      parser.on("-v", "--version", "Print version") { print_version }
+      parser.on("-h", "--help", "Show this help") { print_help(parser) }
+    end
+  end
+
+  private def print_version
+    puts VERSION
+    exit 0
+  end
+
+  private def print_help(parser)
+    puts parser
+    exit 0
   end
 end
