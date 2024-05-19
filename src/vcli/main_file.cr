62d29d83a1260ae63c2d39c8b998e94516a697d7
kebasyaty <kebasyaty@gmail.com>
2024-05-19 08:51:43 +0300

    update

--- src/vcli/main_file.cr
+++ src/vcli/main_file.cr
@@ -4,7 +4,7 @@ module VizborCLI::MainFile
   def modify_main_file(app_name : String) : Nil
     path = Path.new("src/#{app_name}.cr")
     main_file : String = File.read(path)
-    check_main_file(main_file)
+    check_main_file(main_file, app_name)
     arr = main_file.split("\n")
     import = "require \"vizbor\"\n" \
              "require \"./#{app_name}/settings\"\n" \
@@ -15,9 +15,11 @@ module VizborCLI::MainFile
     File.write(path, arr.join)
   end
 
-  private def check_main_file(main_file : String) : Nil
+  private def check_main_file(main_file : String, app_name : String) : Nil
     if main_file.empty?
-      puts "The main project file must not be empty!"
+      STDERR.puts "ERROR: The main project file must not be empty! -> " \
+                  "src/#{app_name}.cr"
+        .colorize.fore(:red).mode(:bold)
       exit 1
     end
   end
