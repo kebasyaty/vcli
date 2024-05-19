f0d10535c6eb970c29659b2bc0b766de812b9a56
kebasyaty <kebasyaty@gmail.com>
2024-05-19 08:34:45 +0300

    update

--- src/vcli/main_file.cr
+++ src/vcli/main_file.cr
@@ -3,9 +3,13 @@ module VizborCLI::MainFile
 
   def modify_main_file(app_name : String) : Nil
     main_file : String = File.read("src/#{app_name}.cr")
-    import = "require \"./#{app_name}/settings\"\n" \
-             "require \"./#{app_name}/services/**\"\n" \
-             "require \"vizbor\"\n\n"
+    main_file_arr = main_file.split("\n")
+    import = "require \"vizbor\"\n" \
+             "require \"./#{app_name}/settings\"\n" \
+             "require \"./#{app_name}/services/**\"\n\n"
+    main_file_arr[0] = import
+    main_file_arr[4] = "  # Start Web Server.\n" \
+                       "  Vizbor::Server.run"
     File.write("src/#{app_name}.cr", "#{import}\n\n#{main_file}")
   end
 end
