50e95589cbc4c37b87e4543acc7544273c9528db
kebasyaty <kebasyaty@gmail.com>
2024-05-20 14:08:21 +0300

    add directories

--- src/vcli/required_directories.cr
+++ src/vcli/required_directories.cr
@@ -3,40 +3,15 @@ module VizborCLI::RequiredDirectories
 
   def add : Nil
     directories = {
-      "assets.zip"    => "https://raw.githubusercontent.com/kebasyaty/vizbor-assets/main/assets.zip",
-      "config.zip"    => "https://raw.githubusercontent.com/kebasyaty/vizbor-assets/main/config.zip",
-      "templates.zip" => "https://raw.githubusercontent.com/kebasyaty/vizbor-assets/main/templates.zip",
+      "lib/vcli/directories/assets"    => "assets",
+      "lib/vcli/directories/config"    => "config",
+      "lib/vcli/directories/templates" => "templates",
+      "lib/vcli/directories/.vscode"   => ".vscode",
+      "lib/vcli/directories/.github"   => ".github",
     }
 
-    directories.each do |path, link|
-      # Download zip archive
-      download_zip(link, path)
-      # Unpack zip archive.
-      unpack_zip(path)
+    directories.each do |src_path, dest_path|
+      FileUtils.cp_r(src_path, dest_path)
     end
-  end
-
-  private def download_zip(download_link : String, save_path : String) : Nil
-    HTTP::Client.get(download_link) do |response|
-      unless response.status.success?
-        raise "error response"
-      end
-      File.open(save_path, "w") { |file_io| IO.copy response.body_io, file_io }
-    end
-  end
-
-  private def unpack_zip(zip_file : String) : Nil
-    Compress::Zip::File.open(zip_file) do |file|
-      file.entries.each do |entry|
-        if entry.file?
-          dir_path = File.dirname(entry.filename)
-          Dir.mkdir_p(dir_path) unless Dir.exists?(dir_path)
-          entry.open do |io|
-            File.write(entry.filename, io.gets_to_end)
-          end
-        end
-      end
-    end
-    File.delete(zip_file)
   end
 end
