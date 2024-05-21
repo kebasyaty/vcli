5ba25067c031bbd6ad6652dcea57421fd2500637
kebasyaty <kebasyaty@gmail.com>
2024-05-21 07:41:25 +0300

    update

--- src/vcli/mongo_options.cr
+++ src/vcli/mongo_options.cr
@@ -45,7 +45,7 @@ module VizborCLI::MongoOptions
     ))
 
   # Default options.
-  @@raw_options : RawMongoDriverOptions = {
+  @@raw_default_options : RawMongoDriverOptions = {
     uri:     "mongodb://localhost:27017",
     options: {
       appname:                                  nil,
@@ -98,55 +98,58 @@ module VizborCLI::MongoOptions
                    "# For reference:\n" \
                    "# https://elbywan.github.io/cryomongo/Mongo/Client.html\n" \
                    "# https://elbywan.github.io/cryomongo/Mongo/Options.html"
-        yaml = @@raw_options.to_yaml
+        yaml = @@raw_default_options.to_yaml
         file.print "#{comments}\n#{yaml}"
       end
     end
   end
 
-  def mongo_driver_options : Mongo::Options
+  def mongo_driver_options : NamedTuple(uri: String, options: Mongo::Options)
     yaml : String = File.read("config/mongo/options.yml")
-    @@raw_options = RawMongoDriverOptions.from_yaml(yaml)
+    raw_options = RawMongoDriverOptions.from_yaml(yaml)
 
-    Mongo::Options.new(
-      appname: @@raw_options[:options][:appname],
-      auth_mechanism: @@raw_options[:options][:auth_mechanism],
-      auth_mechanism_properties: @@raw_options[:options][:auth_mechanism_properties],
-      auth_source: @@raw_options[:options][:auth_source],
-      compressors: @@raw_options[:options][:compressors],
-      connect_timeout: to_span?(@@raw_options[:options][:connect_timeout]),
-      direct_connection: @@raw_options[:options][:direct_connection],
-      heartbeat_frequency: to_span(@@raw_options[:options][:heartbeat_frequency]),
-      journal: @@raw_options[:options][:journal],
-      local_threshold: to_span(@@raw_options[:options][:local_threshold]),
-      max_idle_time: to_span?(@@raw_options[:options][:max_idle_time]),
-      max_pool_size: @@raw_options[:options][:max_pool_size],
-      max_staleness_seconds: @@raw_options[:options][:max_staleness_seconds],
-      min_pool_size: @@raw_options[:options][:min_pool_size],
-      read_concern_level: @@raw_options[:options][:read_concern_level],
-      read_preference: @@raw_options[:options][:read_preference],
-      read_preference_tags: @@raw_options[:options][:read_preference_tags],
-      replica_set: @@raw_options[:options][:replica_set],
-      retry_reads: @@raw_options[:options][:retry_reads],
-      retry_writes: @@raw_options[:options][:retry_writes],
-      server_selection_timeout: to_span(@@raw_options[:options][:server_selection_timeout]),
-      server_selection_try_once: @@raw_options[:options][:server_selection_try_once],
-      socket_timeout: to_span?(@@raw_options[:options][:socket_timeout]),
-      ssl: @@raw_options[:options][:ssl],
-      tls: @@raw_options[:options][:tls],
-      tls_allow_invalid_certificates: @@raw_options[:options][:tls_allow_invalid_certificates],
-      tls_allow_invalid_hostnames: @@raw_options[:options][:tls_allow_invalid_hostnames],
-      tls_ca_file: @@raw_options[:options][:tls_ca_file],
-      tls_certificate_key_file: @@raw_options[:options][:tls_certificate_key_file],
-      tls_certificate_key_file_password: @@raw_options[:options][:tls_certificate_key_file_password],
-      tls_disable_certificate_revocation_check: @@raw_options[:options][:tls_disable_certificate_revocation_check],
-      tls_disable_ocsp_endpoint_check: @@raw_options[:options][:tls_disable_ocsp_endpoint_check],
-      tls_insecure: @@raw_options[:options][:tls_insecure],
-      w: @@raw_options[:options][:w],
-      wait_queue_timeout: to_span?(@@raw_options[:options][:wait_queue_timeout]),
-      w_timeout: to_span?(@@raw_options[:options][:w_timeout]),
-      zlib_compression_level: @@raw_options[:options][:zlib_compression_level],
-    )
+    {
+      uri:     raw_options.uri,
+      options: Mongo::Options.new(
+        appname: raw_options[:options][:appname],
+        auth_mechanism: raw_options[:options][:auth_mechanism],
+        auth_mechanism_properties: raw_options[:options][:auth_mechanism_properties],
+        auth_source: raw_options[:options][:auth_source],
+        compressors: raw_options[:options][:compressors],
+        connect_timeout: to_span?(raw_options[:options][:connect_timeout]),
+        direct_connection: raw_options[:options][:direct_connection],
+        heartbeat_frequency: to_span(raw_options[:options][:heartbeat_frequency]),
+        journal: raw_options[:options][:journal],
+        local_threshold: to_span(raw_options[:options][:local_threshold]),
+        max_idle_time: to_span?(raw_options[:options][:max_idle_time]),
+        max_pool_size: raw_options[:options][:max_pool_size],
+        max_staleness_seconds: raw_options[:options][:max_staleness_seconds],
+        min_pool_size: raw_options[:options][:min_pool_size],
+        read_concern_level: raw_options[:options][:read_concern_level],
+        read_preference: raw_options[:options][:read_preference],
+        read_preference_tags: raw_options[:options][:read_preference_tags],
+        replica_set: raw_options[:options][:replica_set],
+        retry_reads: raw_options[:options][:retry_reads],
+        retry_writes: raw_options[:options][:retry_writes],
+        server_selection_timeout: to_span(raw_options[:options][:server_selection_timeout]),
+        server_selection_try_once: raw_options[:options][:server_selection_try_once],
+        socket_timeout: to_span?(raw_options[:options][:socket_timeout]),
+        ssl: raw_options[:options][:ssl],
+        tls: raw_options[:options][:tls],
+        tls_allow_invalid_certificates: raw_options[:options][:tls_allow_invalid_certificates],
+        tls_allow_invalid_hostnames: raw_options[:options][:tls_allow_invalid_hostnames],
+        tls_ca_file: raw_options[:options][:tls_ca_file],
+        tls_certificate_key_file: raw_options[:options][:tls_certificate_key_file],
+        tls_certificate_key_file_password: raw_options[:options][:tls_certificate_key_file_password],
+        tls_disable_certificate_revocation_check: raw_options[:options][:tls_disable_certificate_revocation_check],
+        tls_disable_ocsp_endpoint_check: raw_options[:options][:tls_disable_ocsp_endpoint_check],
+        tls_insecure: raw_options[:options][:tls_insecure],
+        w: raw_options[:options][:w],
+        wait_queue_timeout: to_span?(raw_options[:options][:wait_queue_timeout]),
+        w_timeout: to_span?(raw_options[:options][:w_timeout]),
+        zlib_compression_level: raw_options[:options][:zlib_compression_level],
+      ),
+    }
   end
 
   private def to_span(value : String) : Time::Span
