76459660b776f3054f6f683cd0da0c5525140172
kebasyaty <kebasyaty@gmail.com>
2024-05-25 11:31:55 +0300

    update

--- src/vcli/required_directories.cr
+++ src/vcli/required_directories.cr
@@ -4,14 +4,14 @@ module VizborCLI::RequiredDirectories
   # Copy folders to the project root.
   def add : Nil
     directories = {
-      "lib/vcli/directories/.github"    => ".github",
-      "lib/vcli/directories/.vscode"    => ".vscode",
-      "lib/vcli/directories/config"     => "config",
-      "lib/vcli/directories/public"     => "public",
-      "lib/vcli/directories/services"   => "services",
-      "lib/vcli/directories/views"      => "views",
-      "lib/vcli/directories/middleware" => "middleware",
-      "lib/vcli/directories/routes"     => "routes",
+      "lib/vcli/directories/.github"  => ".github",
+      "lib/vcli/directories/.vscode"  => ".vscode",
+      "lib/vcli/directories/config"   => "config",
+      "lib/vcli/directories/public"   => "public",
+      "lib/vcli/directories/views"    => "views",
+      "lib/vcli/directories/services" => "services",
+      "directories/middleware.cr"     => "middleware.cr",
+      "directories/routes.cr"         => "routes.cr",
     }
 
     directories.each do |src_path, dest_path|
