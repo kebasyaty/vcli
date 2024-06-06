19e806971a03403f143165fd788b0441315e17fa
kebasyaty <kebasyaty@gmail.com>
2024-06-06 09:39:59 +0300

    update

--- src/vcli/app_state.cr
+++ src/vcli/app_state.cr
@@ -8,43 +8,49 @@ module VizborCLI::AppState
 module Vizbor::Settings
   extend self
 
-  # KEMAL PARAMETERS
-  # ----------------------------------------------------------------------------
-  # Static File Options.
-  # https://kemalcr.com/guide/
-  # Example: {"gzip" => true, "dir_listing" => false}
-  class_getter! static_file_options : Hash(String, Bool)
-  # Disable Static Files.
-  class_getter? disable_static_files : Bool = false
-  # Using Reuse Port for Multiple Kemal Processes.
-  # https://kemalcr.com/cookbook/reuse_port/
-  class_getter? server_reuse_port : Bool = false
-  # Use Logging?
-  # https://kemalcr.com/guide/
-  # You can add logging statements to your code:
-  # Example: Log.info { "Log message with or without embedded \#{variables}" }
-  class_getter? use_logging : Bool = true
+  module Kemal
+    extend self
+    # Static File Options.
+    # NOTE: https://kemalcr.com/guide/
+    # NOTE: Example: {"gzip" => true, "dir_listing" => false}
+    class_getter! static_file_options : Hash(String, Bool)
+    # Disable Static Files.
+    class_getter? disable_static_files : Bool = false
+    # Using Reuse Port for Multiple Kemal Processes.
+    # NOTE: https://kemalcr.com/cookbook/reuse_port/
+    class_getter? server_reuse_port : Bool = false
+    # Use Logging?
+    # NOTE: https://kemalcr.com/guide/
+    # NOTE: You can add logging statements to your code:
+    # NOTE: Example: Log.info { "Log message with or without embedded \#{variables}" }
+    class_getter? use_logging : Bool = true
+  end
 
-  # VIZBOR PARAMETERS
-  # ----------------------------------------------------------------------------
   # If true,
   # an exception page is rendered when an exception is raised which provides a
   # lot of useful information for debugging.
   class_getter? debug : Bool = true
-  # Maximum 44 characters.
+  # WARNING: Maximum 44 characters.
   class_getter app_name : String = "#{db_app_name}"
-  # Match regular expression: /^[a-zA-Z0-9]{16}$/
-  # To generate a key (This is not an advertisement): https://randompasswordgen.com/
+  # WARNING: Match regular expression: /^[a-zA-Z0-9]{16}$/
+  # NOTE: To generate a key (This is not an advertisement): https://randompasswordgen.com/
   class_getter unique_app_key : String = "#{generate_unique_app_key}"
-  # Maximum 60 characters.
-  # Hint: If the line is empty, the name will be generated automatically.
+  # WARNING: Maximum 60 characters.
+  # WARNING: If the line is empty, the name will be generated automatically.
   class_getter database_name : String = ""
-  # https://github.com/crystal-i18n/i18n
+  # NOTE: https://github.com/crystal-i18n/i18n
   class_getter default_locale : Symbol = :en
   # Security
-  # To generate a key (This is not an advertisement): https://randompasswordgen.com/
-  # Minimum 64 characters.
+  # WARNING: Minimum 64 characters.
   class_getter secret_key : String = "#{Random::Secure.hex(64)}"
+
+  # Administrator production email.
+  # WARNING: Maximum 320 characters.
+  class_getter admin_prod_email = "???"
+  # Administrator production password.
+  # WARNING: Default number of characters: max = 256, min = 8
+  # NOTE: To generate a key (This is not an advertisement): https://randompasswordgen.com/
+  class_getter admin_prod_pass = "???"
 
   # URI Scheme
   def scheme : String
