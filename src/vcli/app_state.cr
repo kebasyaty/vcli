332d95c7b046f254194f6ec6ee5b7952f33e96f4
kebasyaty <kebasyaty@gmail.com>
2024-06-06 10:18:59 +0300

    update app_state.cr

--- src/vcli/app_state.cr
+++ src/vcli/app_state.cr
@@ -77,7 +77,11 @@ module Vizbor::Settings
 
   # Application URL
   def app_url : String
-    scheme + "://" + host + ":" + port.to_s
+    if !@@debug
+      scheme + "://" + host
+    else
+      scheme + "://" + host + ":" + port.to_s
+    end
   end
 end
 )
