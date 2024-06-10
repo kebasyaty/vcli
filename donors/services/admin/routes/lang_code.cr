03b56a184acb73329bdf127401c872cb18faab84
kebasyaty <kebasyaty@gmail.com>
2024-06-10 10:53:47 +0300

    update donors

--- donors/services/admin/routes/lang_code.cr
+++ donors/services/admin/routes/lang_code.cr
@@ -2,7 +2,7 @@ module Vizbor::Services::Admin::Routes
   # Get language code
   get "/admin/lang_code" do |env|
     lang_code = {
-      language_code: Vizbor::Settings.default_locale,
+      lang_code: Vizbor::Settings.default_locale,
       msg_err:       "",
     }.to_json
     env.response.content_type = "application/json"
