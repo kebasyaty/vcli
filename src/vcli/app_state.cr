646dc3a93a1284bfaee02144ddd060b264de04f9
kebasyaty <kebasyaty@gmail.com>
2024-11-12 17:58:01 +0200

    update

--- src/vcli/app_state.cr
+++ src/vcli/app_state.cr
@@ -97,7 +97,7 @@ end
   private def generate_unique_app_key : String
     result : String = ""
     # Shuffle symbols in random order.
-    shuffled_chars : Array(String) = ALPHANUMERIC_CHARS.split("").shuffle
+    shuffled_chars : Array(String) = ALPHANUMERIC_CHARS.split("").shuffle!
     chars_count : Int32 = shuffled_chars.size - 1
     size : Int32 = 16
     size.times do
